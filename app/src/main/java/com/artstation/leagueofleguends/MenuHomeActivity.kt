3a23d175b245996ab17f351cda40d8a26d392e03
sebastian vanegas <sergioi.robledoo@utadeo.edu.co>
2022-11-30 21:24:38 -0500

    cambios menu lol

--- app/src/main/java/com/artstation/leagueofleguends/MenuHomeActivity.kt
+++ app/src/main/java/com/artstation/leagueofleguends/MenuHomeActivity.kt
@@ -3,24 +3,22 @@ package com.artstation.leagueofleguends
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.artstation.leagueofleguends.databinding.ActivityMainBinding
+import com.artstation.leagueofleguends.databinding.ActivityMenuHomeBinding
 
-class LoginActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+class MenuHomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMenuHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMenuHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
-
     override fun onResume() {
         super.onResume()
         onclick()
     }
-
     private fun onclick() {
-        binding.btnLogin.setOnClickListener {
-            startActivity(Intent(this, MenuHomeActivity::class.java))
+        binding.btnchamps.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
             finish()
         }
     }
